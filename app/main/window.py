c64a8b8449b48309beec40570ce11ae0e16af906
Leonardo Martella <leonardomartella2002@gmail.com>
2020-07-12 19:29:00 +0200

    Create the main window

---
+++ app/main/window.py
@@ -0,0 +1,16 @@
+from PyQt5.QtWidgets import QMainWindow
+
+from app.main.ui import Ui_MainWindow
+
+
+class MainWindow(QMainWindow):
+    """The application's main window."""
+
+    def __init__(self, parent=None):
+        """Initialize the main window.
+
+        Create and set up the main window's user interface.
+        """
+        super().__init__(parent)
+        self.ui = Ui_MainWindow()
+        self.ui.setupUi(self)
