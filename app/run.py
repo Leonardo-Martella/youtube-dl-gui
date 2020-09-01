2a313e434ba07141e0361c9d6c1ac6af54812456
Leonardo Martella <leonardomartella2002@gmail.com>
2020-09-01 21:03:52 +0200

    Exit the app and set 'stop_event' from 'main'

--- app/run.py
+++ app/run.py
@@ -1,11 +1,19 @@
 import sys
 
 from app.application import Application
+from app.main.download import stop_event
 
 
 def main():
     app = Application(sys.argv)
-    app.run()
+    try:
+        rc = app.exec_()
+    except Exception:
+        rc = 1
+    finally:
+        stop_event.set()
+        print("set stop_event")
+        sys.exit(rc)
 
 
 if __name__ == "__main__":
